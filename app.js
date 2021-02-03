f2a24d4bacb9909dc6dad7cc696879dc4c2986be
SEPR <rasulsaparbaev@mail.ru>
2021-02-03 11:00:13 +0500

    part 2

--- app.js
+++ app.js
@@ -8,11 +8,34 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', "");
-b = prompt('На сколько оцените его?', "");
-c = prompt('Один из последних просмотренных фильмов?', "");
-d = prompt('На сколько оцените его?', "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (i = 0; i < 2; i++) {
+  const a = prompt('Один из последних просмотренных фильмов?', "");
+  b = prompt('На сколько оцените его?', "");
+  
+  if (a != null && b != null && a.length < 50 && b != '' && a != '') {
+    personalMovieDB.movies[a] = b;
+    console.log("done");
+  } 
+  else {
+    console.log('eror');
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0){
+console.log("Мало фильмов смотришь");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30){
+  console.log("Вы псих");
+} else if (personalMovieDB.count === 0){
+  console.log("Вы воопще фильмы не смотрите");
+} else if (personalMovieDB.count < 0){
+  console.log("Введено некорректное число");
+} else {
+  console.log("Eror");
+}
+
+
 console.log(personalMovieDB);
